ec22032a58e9d4cb21696cb9ed889c90360d89da
Paul McCartney <paulmc@1791.com>
2023-01-02 17:10:26 -0500

    Add overview map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverviewMapComponent } from './overview-map/overview-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OverviewMapComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/overview-map/overview-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-map',
+  templateUrl: './overview-map.component.html',
+  styleUrls: ['./overview-map.component.scss']
+})
+export class OverviewMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
